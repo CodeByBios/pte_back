f67938c60e7b414c2fb3057df7fc5e9aed701955
michael.tongle <tonglemichael@yahoo.fr>
2018-07-10 10:30:28 +0200

    Init app pte

---
+++ src/main/java/com/sodifrance/pte/PteApplication.java
@@ -0,0 +1,14 @@
+package com.sodifrance.pte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PteApplication.class, args);
+		
+		System.out.println("test app");
+	}
+}
