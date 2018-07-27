69c1812dbe50c2b8d85d3ad08c819a6feda66db6
brice <kapchej@gmail.com>
2018-07-27 14:37:10 +0200

    Signed-off-by: brice <kapchej@gmail.com>

--- src/main/java/com/sodifrance/pte/service/impl/CreateTestServiceImpl.java
+++ src/main/java/com/sodifrance/pte/service/impl/CreateTestServiceImpl.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.google.common.collect.Sets;
 import com.sodifrance.pte.dao.LangageDao;
 import com.sodifrance.pte.dao.NiveauDao;
 import com.sodifrance.pte.dao.TypeQuestionDao;
